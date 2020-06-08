f5559ac08f77390107d683dcc9171911c9867ea0
papjco <papjco@gmail.com>
2020-06-07 21:51:58 -0400

    fixed "deploy.js"

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo "Installing 'node-cmd'"
 npm install node-cmd
 echo "Installing 'node-ssh'"
 npm install node-ssh
-#echo "Running 'deploy.js'"
-#node deploy.js
+echo "Running 'deploy.js'"
+node deploy.js
